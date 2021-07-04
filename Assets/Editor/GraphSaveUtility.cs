cc353e21cefb58e35612c84ae8f668049477bc8e
hwx0000 <609987101@qq.com>
2021-07-04 14:40:15 +0800

    Modify GraphSaveUtility class to be a singleton

--- Assets/Editor/GraphSaveUtility.cs
+++ Assets/Editor/GraphSaveUtility.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GraphSaveUtility
 {
-    public static void SaveData()
+    private DialogueGraphView _dialogueGraphView;
+
+    // 每次从类里获取edges和nodes时，都会去取graphView里的内容，并进行转型
+    private List<Edge> edges => _dialogueGraphView.edges.ToList();
+    private List<DialogueNode> nodes => _dialogueGraphView.nodes.ToList().Cast<DialogueNode>().ToList();
+
+    public static GraphSaveUtility GetInstance(DialogueGraphView graphView)
+    {
+        return new GraphSaveUtility
+        {
+            _dialogueGraphView = graphView
+        };
+    }
+
+    public void SaveData()
     {
 
     }
 
-    public static void LoadData()
+    public void LoadData()
     {
 
     }
